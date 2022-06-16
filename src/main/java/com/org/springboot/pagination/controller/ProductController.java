01d157d774a98c9528748e961832253bab311cf7
Amlan Deep Nandi <Amlandeep001@users.noreply.github.com>
2022-06-17 02:19:55 +0530

    Changes in Controller
    Logger changes

--- src/main/java/com/org/springboot/pagination/controller/ProductController.java
+++ src/main/java/com/org/springboot/pagination/controller/ProductController.java
@@ -39,10 +39,11 @@ public class ProductController {
 	
 	@GetMapping("/{field}")
 	private APIResponse<List<Product>> getProductsWithSort(@PathVariable String field){
-		
-		log.info("Displaying products after sorting in Ascending order on basis of: {}", field);
-		
+
 		List<Product> allProducts = service.findProductsWithSorting(field);
+		
+		log.info("Displaying products after sorting in Ascending order on the basis of: {}", field);
+		
 		return new APIResponse<>(allProducts.size(), allProducts);
 		
 	}
@@ -50,10 +51,11 @@ public class ProductController {
 	
 	@GetMapping("/pagination/{offset}/{pageSize}")
 	private APIResponse<Page<Product>> getProductsWithPagination(@PathVariable int offset, @PathVariable int pageSize){
+
+		Page<Product> productsWithPagination = service.findProductsWithPagination(offset, pageSize);
 		
 		log.info("Displaying products after pagination where page number: {} and count in per page: {}", offset, pageSize);
 		
-		Page<Product> productsWithPagination = service.findProductsWithPagination(offset, pageSize);
 		return new APIResponse<>(productsWithPagination.getSize(), productsWithPagination);
 		
 	}
